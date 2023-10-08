0d54287c7e885ec230087b7da2f50a7b389daebb
sana197111 <yc0047@gmail.com>
2023-10-09 08:11:28 +0900

    result 코드 추가

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import Type1 from './type/type1.png';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Result() {
+    const [type, setType] = useState(1);
+
+    useEffect(() => {
+        // Fetch the scores from the server when component mounts
+        axios.get('/api/scores')
+            .then(response => {
+                // response.data contains the returned data
+                const data = response.data;
+
+                // Find the index of the max score
+                const maxIndex = Object.keys(data).reduce((a, b) => data[a] > data[b] ? a : b);
+
+                // Set the type state
+                setType(parseInt(maxIndex.replace('score', '')));
+            })
+            .catch(error => console.error('Error:', error));
+    }, []);
+
     return (
         <div className="min-h-screen p-4 overflow-y-auto max-h-screen flex flex-col items-center" style={{backgroundColor: "#514d4c"}}>
-            {/* <h1 className="mb-4 text-center text-4xl" style={{color: "#d2d6d5"}}>
-                결과입니다
-            </h1> */}
-            <img src={Type1} alt="Type 1" className="object-contain" />
+            <img src={require(`./type/type${type}.png`).default} alt={`Type ${type}`} className="object-contain" />
         </div>
     );
 }
