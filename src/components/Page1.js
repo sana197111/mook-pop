d7b6022b35280c52ef2ae725f65380078a2301d2
sana197111 <yc0047@gmail.com>
2023-10-09 07:35:34 +0900

    page1 수정

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -51,13 +51,6 @@ function Page1() {
                 <br/>
                 <span className="text-customYellow">#휴식 #Rest #명상 #비워내기</span><br/>
             </p>
-            <button
-                className="mt-8 mb-8 px-6 py-2 border rounded hover:bg-gray-500 hover:text-white active:bg-gray-700 active:text-white transition duration-300 ease-in-out" 
-                style={{ color: "#d2d6d5", borderColor: "#d2d6d5" }}
-                onClick={startTimer}
-            >
-                시작하기
-            </button>
             <AnimatePresence>
                 {showNextPageMessage && (
                         <motion.p
@@ -90,6 +83,13 @@ function Page1() {
                     {renderTime}
                 </CountdownCircleTimer>
             </AnimatePresence>
+            <button
+                className="mt-8 mb-8 px-6 py-2 border rounded hover:bg-gray-500 hover:text-white active:bg-gray-700 active:text-white transition duration-300 ease-in-out" 
+                style={{ color: "#d2d6d5", borderColor: "#d2d6d5" }}
+                onClick={startTimer}
+            >
+                시작하기
+            </button>
         </div>
     );
 }
